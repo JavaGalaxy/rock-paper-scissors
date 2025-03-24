1d9cc32b5a680ab73bc6ef89ff039903dd96b7a3
Amogh Shendre <amogh@thefword.ai>
2025-03-24 07:10:46 -0500

    Initialize project using create react app and generate a random move to be displayed by computer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+type Move = 'rock' | 'paper' | 'scissors';
+const Moves: readonly Move[] = ['rock', 'paper', 'scissors'];
+
+const getRandomMove = (): Move => {
+  const index = Math.floor(Math.random() * Moves.length);
+  return Moves[index];
+}
+
 function App() {
+  const[computerMove, setComputerMove] = useState<Move>(getRandomMove());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Computer move: {computerMove}
     </div>
   );
 }
