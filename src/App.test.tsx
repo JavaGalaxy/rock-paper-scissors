eb3ef0f611b82ddf4cae182923837b4b9e250085
Amogh Shendre <amogh@thefword.ai>
2025-03-25 17:46:07 -0500

    unit test added to see what computer chose

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Rock Paper Scissors App', () => {
+  test('renders computer move text', () => {
+    render(<App />);
+    const computerMoveElement = screen.getByText(/Computer chose:/i);
+    expect(computerMoveElement).toBeInTheDocument();
+  });
 });
